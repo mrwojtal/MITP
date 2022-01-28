05c3cf58b2bf619b077d4cb708ddbdd3dced8b1f
mrwojtal <98617467+mrwojtal@users.noreply.github.com>
2022-01-28 22:20:28 +0100

    Create days.h

---
+++ zad13/days.h
@@ -0,0 +1,14 @@
+#ifndef ZAD13_DAYS_H
+#define ZAD13_DAYS_H
+                         //header file for zad13
+struct DAY {
+    int dzien;
+    int miesiac;
+    int rok;
+} DAY1, DAY2;
+
+int liczenie();
+
+int days();
+
+#endif //ZAD13_DAYS_H
