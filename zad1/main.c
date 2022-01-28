1ae2838d5af202165b26b34cc58fbd894600fad1
mrwojtal <98617467+mrwojtal@users.noreply.github.com>
2022-01-28 21:45:48 +0100

    Create main.c

---
+++ zad1/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+                                                /* In this task i print Hello! in polish with %*/
+int main() {
+    printf("\n\t%%%%%%%%%%%%%%%%%%%%%%%%\t\n");
+    printf("\t   Witaj!");
+    printf("\n\t%%%%%%%%%%%%%%%%%%%%%%%%\t\n\n");
+    return 0;
+}
+
+
+
