6a803a96dc9c4fa62852d8bc00e8fe751fb4e0c9
mrwojtal <98617467+mrwojtal@users.noreply.github.com>
2022-01-28 23:25:10 +0100

    Create insert_str.c

---
+++ zad20/zad19a/insert_str.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "sortstring.h"
+                                                            // insert sort algorythm for strings
+void insert(char* tab[], int n) {
+    int i, j;
+    char* wyraz;
+    for(i = 1; i < n; i++) {
+        wyraz = tab[i];
+        for (j = i - 1; j >= 0 && (strcmp(tab[j], wyraz)) > 0; j--) {
+            tab[j + 1] = tab[j];
+        }
+        tab[j + 1] = wyraz;
+    }
+}
