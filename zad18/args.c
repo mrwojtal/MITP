501195763e149636f77458e4e79f24fb5f00f6c2
mrwojtal <98617467+mrwojtal@users.noreply.github.com>
2022-01-28 22:43:05 +0100

    Create args.c

---
+++ zad18/args.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include "sortint.h"
+                                                    // choosing sort algorythm depending on arguments
+int args(int argc, char* argv[]) {
+    int mode = 0;
+    if (argc == 2) {
+        if (strcmp(argv[1], "-nbubble") == 0)
+            mode = 1;
+        else if (strcmp(argv[1], "-nselect") == 0)
+            mode = 2;
+    }
+    else if(argc ==3) {
+        if (strcmp(argv[2], "bubble")==0)
+            mode=1;
+        else if (strcmp(argv[2], "select")==0)
+            mode=2;
+    }
+    return mode;
+}
