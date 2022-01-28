3d97f1e28975ff30bacac7bc5fd6a750ba4f644c
mrwojtal <98617467+mrwojtal@users.noreply.github.com>
2022-01-28 22:50:04 +0100

    Create sortint.h

---
+++ zad18/sortint.h
@@ -0,0 +1,12 @@
+#ifndef SORTINT_H
+#define SORTINT_H
+
+void bubble(int tab[], int n);
+
+void select(int tab[], int n);
+
+void swap(int* x, int* y);
+
+int args(int argc, char* argv[]);
+
+#endif //SORTINT_H
