1b5e8b10c649764a4a30ca54ab3ae2f9efd79194
mrwojtal <98617467+mrwojtal@users.noreply.github.com>
2022-01-28 22:02:19 +0100

    Create zad7.c

---
+++ zad7/zad7.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+                                    // printing ascii number for typed sign
+int main() {
+    char znak;
+    printf("Podaj dowolny znak:");
+    scanf("%c", &znak);
+    printf("ASCII:%d\n", znak);
+    return 0;
+}
